be7c8c3faee8bbf9b76cfaf90549d14c873369d1
Gabridg <gabri99.del@gmail.com>
2022-07-29 16:34:42 +0200

    displaying elements on page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
-
+    <Header></Header>
   </div>
 </template>
 
 <script>
-
+import Header from './components/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
+    Header,
   }
 }
 </script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+            <h1></h1>
+            <div>
+                <input type="text">
+                <button class="btn btn-danger">Cerca</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheHeader",
+}
+</script>
+
+<style lang="scss">
+@import'../assets/Scss/style.scss';
+</style>
